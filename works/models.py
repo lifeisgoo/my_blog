b094359928842632403da6a43669c8758be356fb
lifeisgoo <abdulatibovao@gmail.com>
2022-06-30 21:32:10 +0500

    added works model

--- works/models.py
+++ works/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from ckeditor_uploader.fields import RichTextUploadingField
 
-# Create your models here.
+class WorksCategoryModel(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+    class Meta:
+        verbose_name = 'work category'
+        verbose_name_plural = 'works category'
+
+
+class WorksModel(models.Model):
+    category = models.ManyToManyField(WorksCategoryModel)
+    title = models.CharField(max_length=255)
+    body = RichTextUploadingField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    work_image = models.ImageField(upload_to='works/')
+
+    def __str__(self):
+        return self.title
+
+
+    class Meta:
+        verbose_name = 'work'
+        verbose_name_plural = 'works'
+
+
